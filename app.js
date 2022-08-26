4bf5f09de21c575c1f55dcf7b736f50839a29012
SFMZ21 <smartinezz1@miumg.edu.gt>
2022-08-26 02:52:56 -0600

    Agregando rutas

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const app= express()
 app.use(cors())
 const port= process.env.PORT || 3000
 
+/*Aqui invocamos a las rutas :)
+*/
+
+app.use("/api",require("./routes"))
+
+
+
 app.listen(port, () =>(
     console.log(`http://localhost:${port}`)
 ))
