63bdb4604c20a3e61097f518582d95939c517d29
SFMZ21 <smartinezz1@miumg.edu.gt>
2022-08-26 01:10:30 -0600

    Agregando el proyecto

---
+++ app.js
@@ -0,0 +1,13 @@
+require("dotenv").config()
+const express = require('express')
+const cors = require('cors')
+const dbConnnect =require('./config/mongo')
+const app= express()
+app.use(cors())
+const port= process.env.PORT || 3000
+
+app.listen(port, () =>(
+    console.log(`http://localhost:${port}`)
+))
+
+dbConnnect()
