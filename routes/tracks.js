f4c899099ebf4eaa3b8d7264aed682417a3e4441
SFMZ21 <smartinezz1@miumg.edu.gt>
2022-09-17 00:04:34 -0600

    Documentación de tracks terminada

--- routes/tracks.js
+++ routes/tracks.js
@@ -5,18 +5,72 @@ const {validatorCreateItem, validatorGetItem}=require("../validators/tracks");
 const { getItems, getItem, createItem, updateItem, deleteItem } = require("../controllers/tracks");
 const checkRol = require("../middleware/rol");
 
-/***
- * Lista de los items
+/**
+ * Get all from tracks
+ * @openapi
+ * /tracks:
+ *      get:
+ *          tags:
+ *              - tracks
+ *          summary: "Todas las tracks"
+ *          description: "Obtener todas las tracks "
+ *          security:
+ *              - bearerAuth: []
+ *          responses:
+ *                  '201':
+ *                      description: "las tracks se obtuvieron"
+ *                  '403':
+ *                      description: "Error al obtener la data"
  */
 router.get("/",authMiddleware,getItems);
 
-/***
- * obtener detalle de  item
+/**
+ * Get details from tracks
+ * @openapi
+ * /tracks/{id}:
+ *      get:
+ *          tags:
+ *              - tracks
+ *          summary: "Detalle tracks"
+ *          description: "Obtener el detalle de un track"
+ *          security:
+ *              - bearerAuth: []
+ *          parameters:
+ *          - name: id
+ *            in: path
+ *            description: ID de track a retornar
+ *            required: true
+ *            schema:
+ *              type: string
+ *          responses:
+ *                  '201':
+ *                      description: "El tracks se obtuvo"
+ *                  '403':
+ *                      description: "Error al obtener el track"
  */
  router.get("/:id",authMiddleware,validatorGetItem,getItem);
 
 /**
- * crear un registro/item
+ * create new track
+ * @openapi
+ * /tracks:
+ *      post:
+ *          tags:
+ *              - tracks
+ *          summary: "Registrar nuevo track"
+ *          description: "Esta ruta es para crear  un nuevo track"
+ *          security:
+ *              - bearerAuth: []
+ *          requestBody:
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: "#/components/schemas/tracks"
+ *          responses:
+ *                  '201':
+ *                      description: "El track se registra correctamente"
+ *                  '403':
+ *                      description: "Error al registrar el track"
  */
 router.post("/",
 authMiddleware,
@@ -25,12 +79,59 @@ validatorCreateItem,
 createItem);
 
 /**
- * actualizar registro
+ * update track
+ * @openapi
+ * /tracks/{id}:
+ *      put:
+ *          tags:
+ *              - tracks
+ *          summary: "actualizar  track"
+ *          description: "Esta ruta es para actualizar la data de un track"
+ *          security:
+ *              - bearerAuth: []
+ *          parameters:
+ *          - name: id
+ *            in: path
+ *            description: ID de track a retornar
+ *            required: true
+ *            schema:
+ *              type: string
+ *          requestBody:
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: "#/components/schemas/tracks"
+ *          responses:
+ *                  '201':
+ *                      description: "El track se actualiza correctamente"
+ *                  '403':
+ *                      description: "Error al actualizar el track"
  */
  router.put("/:id",authMiddleware,validatorGetItem ,validatorCreateItem,updateItem);
 
- /***
- * Lista de los items
+ /**
+ * Delete details from tracks
+ * @openapi
+ * /tracks/{id}:
+ *      delete:
+ *          tags:
+ *              - tracks
+ *          summary: "Eliminar track"
+ *          description: "elimina un track"
+ *          security:
+ *              - bearerAuth: []
+ *          parameters:
+ *          - name: id
+ *            in: path
+ *            description: ID de track a eliminar
+ *            required: true
+ *            schema:
+ *              type: string
+ *          responses:
+ *                  '201':
+ *                      description: "El track se elimino"
+ *                  '403':
+ *                      description: "Error al eliminar el track"
  */
 router.delete("/:id",authMiddleware,validatorGetItem,deleteItem);
 
